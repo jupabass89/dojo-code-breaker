75c26d51dcadef466dde77971c34c32e16385b68
Jaime Jimenez Serrano <jaime.jimenezs@udea.edu.co>
2019-09-07 10:39:27 -0500

    coverage

--- app/app.js
+++ app/app.js
@@ -14,4 +14,15 @@ app.get('/guess/:number', function(req, response){
     response.send({result: CodeBreaker.codeBreaker(number)})
 })
 
+app.get('/anything/:number', function(req, response){
+    number = req.params.number;
+    response.status(201).send(number);
+})
+
+
+app.get('/nothing/:number', function(req, response){
+    number = req.params.number;
+    response.status(400).send(number);
+})
+
 module.exports = app;
